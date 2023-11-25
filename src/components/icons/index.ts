ca90c3aa5cc9e7ab0419bb16bda102499c0d064a
Maryna Dubchak <dubchmar@gmail.com>
2023-11-25 16:35:54 +0200

    add icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,3 +3,6 @@ export { default as FullscreenIcon } from './fullscreen.svg';
 export { default as ScreenIcon } from './screen.svg';
 export { default as CloseIcon } from './close.svg';
 export { default as Logo } from './logo.svg';
+export { default as FileIcon } from './file.svg';
+export { default as FolderIcon } from './folder.svg.svg';
+export { default as EmptyFolderIcon } from './empty-folder.svg';
